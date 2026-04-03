d9766e60b7ffadf4daea3fc1d04e070ed60b0ee9
Cherry <cherry.ching23@imperial.ac.uk>
2026-04-03 16:43:24 +0100

    Initial Commit

---
+++ GaitNav/GaitNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaitNavApp.swift
+//  GaitNav
+//
+//  Created by Cherry on 3/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct GaitNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
